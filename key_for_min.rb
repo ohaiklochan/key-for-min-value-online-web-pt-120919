5f5c4a7b5867338f84874360aef0d824cf8b2c40
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 02:57:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,7 @@ def key_for_min_value(name_hash)
   smallest_key = nil
   name_hash.each do |key, value|
     if smallest_value == 0 || value < smallest_value
-      
+      smallest_value = value
+      smallest_key = key
+    
 end
