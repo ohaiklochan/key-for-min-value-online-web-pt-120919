b09459dd80947824b53bb148f43c0194ce60345a
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 02:58:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,5 +8,7 @@ def key_for_min_value(name_hash)
     if smallest_value == 0 || value < smallest_value
       smallest_value = value
       smallest_key = key
-    
+    end
+  end
+  smallest_key
 end
